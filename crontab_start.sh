f575ee3e5306f2a0c6a2cc12d8f0c14cea3c26c9
BloodyOcean <072mortedwardedik1337>
2022-07-20 12:20:05 +0300

    Aasdasd

--- crontab_start.sh
+++ crontab_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pem_path = $1
+echo $pem_path
 
 #install necessary packages
 sudo apt-get update
@@ -10,11 +11,6 @@ sudo apt-get install openjdk-8-jre
 sudo apt-get install awscli
 sudo apt-get install cron
 sudo apt-get install unzip
-
-
-#get zipped repository
-#wget -P ~/ https://github.com/BloodyOcean/aws_task3/archive/refs/heads/main.zip
-#unzip main.zip -d res
 
 #create folder for crontab logs
 mkdir /tmp/crontab_logs
@@ -33,9 +29,9 @@ aws configure
 crontab -l > mycron
 
 #echo new crons into cron file
-echo "20 * * * * /usr/bin/python3 /home/ubuntu/res/aws_task3-main/part2_spark/main.py >> /tmp/crontab_logs/spark-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
-echo "*/30 * * * * /usr/bin/python3 /home/ubuntu/res/aws_task3-main/part1_db/main.py --transactions >> /tmp/crontab_logs/transactions-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
-echo "0 * * * * /usr/bin/python3 /home/ubuntu/res/aws_task3-main/part1_db/main.py --peoplecards 10 >> /tmp/crontab_logs/peoplecards-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
+echo "20 * * * * /usr/bin/python3 /home/ubuntu/aws_task3-main/part2_spark/main.py >> /tmp/crontab_logs/spark-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
+echo "*/30 * * * * /usr/bin/python3 /home/ubuntu/aws_task3-main/part1_db/main.py --transactions >> /tmp/crontab_logs/transactions-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
+echo "0 * * * * /usr/bin/python3 /home/ubuntu/aws_task3-main/part1_db/main.py --peoplecards 10 >> /tmp/crontab_logs/peoplecards-`date +\%Y\%m\%d\%H\%M\%S`.log 2>&1" >> mycron
 
 #install new cron file
 crontab mycron
